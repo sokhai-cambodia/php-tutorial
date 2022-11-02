17bccb469cb62ebceb603a29f436719902835a02
hai <hai.sok@wingmoney.com>
2022-11-02 08:39:27 +0700

    add echo

---
+++ 2-echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2- Echo</h1>
+    <?php 
+        // php code
+        // echo "what we want to echo";
+        echo "I am learning php";
+        echo "<p>This is paragraph from echo</p>";
+        echo 1 + 1;
+        echo "<br>";
+        echo 19.0;
+    ?>
+
+    <p>This is paragraph from html</p>
+</body>
+</html>
