9c950266ce4425a8d5e0b5309891d7915162593c
hai <hai.sok@wingmoney.com>
2022-11-02 08:52:18 +0700

    add variable

---
+++ 3-variable.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>3- Variable</h1>
+    <?php 
+        // $variable_name = 'value';
+        // $salary = 250;
+        $name = 'Tey Tey'; // string
+        $weight = 37.5; // float
+        $height = 155; // integer
+        $age = 18; // integer
+        $is_single = false; // boolean
+        // $is_single = true;
+        echo "<p>My crush name: ".$name."</p>";
+        echo "<p>My crush weight: ".$weight."</p>";
+        echo "<p>My crush height: ".$height."</p>";
+        echo "<p>My crush age: ".$age."</p>";
+        echo "<p>My crush is_single: ".$is_single."</p>";
+    ?>
+</body>
+</html>
